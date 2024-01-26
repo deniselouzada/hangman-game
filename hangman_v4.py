9577220c7633b78c674c92963fdd02b77c9b82b1
deniselouzada <louzadadenise@gmail.com>
2024-01-26 13:43:14 -0300

    finished oop version

--- hangman_v4.py
+++ hangman_v4.py
@@ -3,12 +3,13 @@
 
 # Import
 import random
-from os import system, name
+import clear_screen as clear
 
+# Lista de palavras
+word_list = ['manga', 'goiaba', 'uva', 'geladeira', 'banana', 'melancia', 'carro', 'aviao', 'livro', 'casa']
+ 
 # Board (tabuleiro)
 board = ['''
-
->>>>>>>>>>Hangman<<<<<<<<<<
 
 +---+
 |   |
@@ -71,63 +72,77 @@ O   |
 class Hangman:
 
 	# Método Construtor
-    def __init__(self, word_list):
-        self.word_list = word_list
+    def __init__(self):
+        clear
         self.board = board
-        self.word = random.choice(word_list)
         self.uncovered = []
         self.wrong = []
-        self.chances = len(self.word)
-        self.clear_screen()
-
-    def clear_screen(self):
-        if name == 'nt':
-            _ = system('cls')
-        else:
-            _ = system('clear')
+        self.chances = 6
+        self.board_index = 0
             
-	# Método para adivinhar a letra
-    def guess(self, uncovered):
-        self.guess = input('Choose a letter: ').lower()
-        self.wrong = []
-        if self.guess in word:
-            i = 0
-            for letter in word:
-                if guess == letter:
-                    uncovered[i] = guess
-                i += 1
+    def choose_word(self, word_list):
+        self.word = random.choice(word_list)
+        self.uncovered = ['_' for letter in self.word]
+        return self.word, self.uncovered
+        
+    def get_guess(self):
+        self.guess = input('\nEscolha uma letra: ').lower()
+        return self.guess
+
+    def check_guess(self):
+        if self.guess in self.wrong:
+            print('Voce ja escolheu essa letra!')
+        elif self.guess in self.word:
+            for i, letter in enumerate(self.word):
+                if self.guess == letter:
+                    self.uncovered[i] = self.guess
         else:
-            wrong.append(guess)
-            print('Letras erradas: ', wrong)
-            chances -= 1
-        
+            self.wrong.append(self.guess)
+            print('Letras erradas: ', self.wrong)
+            self.chances -= 1
+            self.board_index += 1
+        return self.uncovered, self.chances, self.board_index, self.wrong
+            
 	# Método para verificar se o jogo terminou
-    def endgame(self, uncovered):
-        while chances > 0:
-            self.status(uncovered, chances, wrong)
-            guess = get_guess()
-            chances, wrong, uncovered = update_game_status(uncovered, word, guess, chances, wrong)
-            if win(uncovered, word):
-                break
-            else:
-                print("\nVoce perdeu =( A palavra era :", word)
-        
-	# Método para verificar se o jogador venceu
-    def win(self, uncovered):
-        if "_" not in uncovered:
-            print(uncovered)
-            print("\nVoce venceu! A palavra é :", word, "\n") 
+    def endgame(self):
+        if self.win() or self.chances == 0:
             return True
         else:
             return False
         
-	# Método para não mostrar a letra no board
-    def uncovered(self, word):
-        self.uncovered = ['_' for letter in word]
-        print(self.uncovered)
-        
+	# Método para verificar se o jogador venceu
+    def win(self):
+        if "_" not in self.uncovered:
+            print(self.board[self.board_index])
+            print(''.join(self.uncovered))
+            print(f"\nVoce venceu! =D \nA palavra é : {self.word} \n") 
+            return True
+        else:
+            return False
+  
 	# Método para checar o status do game e imprimir o board na tela
-    def status(self, uncovered, chances, wrong):
-        print(uncovered)
-        print('\nVocê tem ', chances, ' chances!')
-        print('Letras erradas: ', wrong)
+    def status(self):
+        print(self.board[self.board_index])
+        print(''.join(self.uncovered))
+        if self.chances == 0:
+            print(f"\nVoce perdeu =( \nA palavra era : {self.word} \n")
+        else:
+            print(f'\nVocê tem {self.chances} chances!')
+            print('Letras erradas: ', self.wrong)
+
+if __name__ == '__main__':
+    game = Hangman()
+    game.choose_word(word_list)
+    
+    while not game.endgame():
+        game.status()
+        game.get_guess()
+        game.check_guess()
+    if not game.win():
+        game.status()
+        
+        
+        
+    
+    
+    
